5b873d8c06c670e95d5351eee0f9d05cb60bdd96
panyue05 <panyue05@baidu.com>
2021-07-09 11:31:49 +0800

    路径总和

---
+++ 112.路径总和.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=java
+ *
+ * [112] 路径总和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class TreeNode {
+    int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+class Solution {
+    boolean isRoot = true;
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) {
+            return false;
+        }
+        if(root.val == targetSum){
+            if((root.left == null) && (root.right==null))
+            return true;
+        }
+        
+        return hasPathSum(root.left,targetSum-root.val) 
+        || hasPathSum(root.right,targetSum-root.val);
+    }
+}
+// @lc code=end
+
